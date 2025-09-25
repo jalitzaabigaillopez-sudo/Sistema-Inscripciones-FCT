79bc71432e8e304baf23949437849e52f5046594
Jalii <jalitzaabigaillopez@gmail.com>
2025-09-24 23:04:26 -0300

    Update index.blade.php

--- resources/views/catalogos/academias/index.blade.php
+++ resources/views/catalogos/academias/index.blade.php
@@ -260,7 +260,7 @@
                                                 required>
                                         </div>
                                         <div class="col-md-6 mb-3">
-                                            <label for="telefonoAcademiaEditar" class="form-label">Teléfon00o <span
+                                            <label for="telefonoAcademiaEditar" class="form-label">Teléfono <span
                                                     class="text-danger">*</span></label>
                                             <input type="text" class="form-control form-control-sm"
                                                 id="telefonoAcademiaEditar" name="telefono"
